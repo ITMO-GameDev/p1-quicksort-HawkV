fa7f98fa14c6c8945b2163295850cef2dbafa666
HawkV <hawkv150@gmail.com>
2019-12-25 04:22:19 +0300

    Update MemoryTest.cpp

--- MemoryTest.cpp
+++ MemoryTest.cpp
@@ -1,1 +1,16 @@
+#include "Memory.cpp"
 
+TEST(AllocationTest, SimpleTest) {
+  MemoryAllocator allocator;
+  allocator.init();
+  
+  int *pi = allocator.alloc(sizeof(int));
+  double *pd = allocator.alloc(sizeof(double));
+  int *pa = allocator.alloc(10 * sizeof(int));
+  
+  allocator.free(pa);
+  allocator.free(pd);
+  allocator.free(pi);
+  
+  allocator.destroy();
+}
